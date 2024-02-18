7c05c48c0e1412600d7fc1ad19baa340850871e4
mernstackjs <mernstack.js@gmail.com>
2024-02-18 14:48:34 +0100

    veweb-client

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <div>BrandName !</div>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
